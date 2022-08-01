6c5ad8ce6b7c1bd9745d170754069eb6e93f97f0
Atrylon <berenger.desgardin@gmail.com>
2022-08-01 15:20:15 +0200

    Forms update + delete

--- merchex/listings/forms.py
+++ merchex/listings/forms.py
@@ -1,8 +1,22 @@
 from unittest.util import _MAX_LENGTH
 from django import forms
 
+from listings.models import Band, Listing
+
 
 class ContactUsForm(forms.Form):
     name = forms.CharField(required=False)
     email = forms.EmailField(required=True)
     message = forms.CharField(max_length=1000)
+
+class BandForm(forms.ModelForm):
+    class Meta:
+        model=Band
+        fields='__all__'
+
+class ListingForm(forms.ModelForm):
+    class Meta:
+        model= Listing
+        fields='__all__'
+
+
--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.core.mail import send_mail
 
 from listings.models import Band, Listing
-from listings.forms import ContactUsForm
+from listings.forms import ContactUsForm, BandForm, ListingForm
 
 
 def index(request):
@@ -24,7 +24,52 @@ def band_detail(request, id):
     return render(request, 
         'listings/band_detail.html',
         {'band':band}
-)
+    )   
+
+def band_create(request):
+
+    if request.method == 'POST':
+        form=BandForm(request.POST)
+
+        if form.is_valid():
+            band = form.save()
+            return redirect('band-detail', band.id)
+
+    else:
+        form=BandForm()
+
+    return render(request, 
+        'listings/band_create.html',
+        {'form':form}
+    )
+
+def band_change(request, id):
+    band = get_object_or_404(Band, id=id)
+
+    if request.method == 'POST':
+        form = BandForm(request.POST, instance = band)
+        if form.is_valid():
+            form.save()
+            return redirect('band-detail', band.id)
+    else:
+        form = BandForm(instance=band)
+
+    return render(request,
+        'listings/band_update.html',
+        {'form': form}
+    )
+
+def band_delete(request, id):
+    band = get_object_or_404(Band, id=id)
+
+    if request.method == 'POST':
+        band.delete()
+        return redirect('band-list')
+
+    return render(request,
+        'listings/band_delete.html',
+        {'band': band}
+    )
 
 def listing_list(request):
     listings =  Listing.objects.all()
@@ -38,6 +83,56 @@ def listing_detail(request, id):
         'listings/listing_detail.html', 
         {'listing': listing}
     )
+
+def listing_create(request):
+
+    if request.method == 'POST':
+        form=ListingForm(request.POST)
+        
+        if form.is_valid():
+            listing = form.save()
+            return redirect('listing-detail', listing.id)
+    else:
+        form=ListingForm()
+
+    return render(request,
+        'listings/listing_create.html',
+        {'form': form}
+    )
+
+def listing_change(request, id):
+    listing = get_object_or_404(Listing, id=id)
+
+    if request.method == 'POST':
+        form=ListingForm(request.POST, instance=listing)
+
+        if form.is_valid():
+            form.save()
+            return redirect('listing-detail', listing.id)
+    else:
+        form = ListingForm(instance=listing)
+
+
+    return render(request,
+        'listings/listing_update.html',
+        {'form': form}
+    )
+
+def listing_delete(request, id):
+    listing = get_object_or_404(Listing, id=id)
+
+    if request.method == 'POST':
+        listing.delete()
+        return redirect('listing-list')
+    else:
+        form = ListingForm(instance=listing)
+
+
+    return render(request,
+        'listings/listing_delete.html',
+        {'listing': listing}
+    )
+
 
 def about(request):
     return render(request, 'listings/about.html')
